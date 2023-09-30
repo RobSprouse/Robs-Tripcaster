0de8c750f4df04188cd365535c19b4db1b20738e
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-09-29 23:47:58 -0400

    added the storage of the cities' lat and lon with their extra info
    added the storage of the cities' lat and lon with their extra info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,107 @@
 $(document).ready(function () {
-  $("#invalidEntry").hide();
-});
+    $("#invalidEntry").hide();
+  });
+  
+  let apiKey = "ae8a9c52dbc61de25a46b1c84484bee2";
+  
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    $("#invalidEntry").text("");
+    let cityName = $("#cityName").val();
+    let getLatLonURL =
+      "http://api.openweathermap.org/geo/1.0/direct?q=" +
+      cityName +
+      "&appid=" +
+      apiKey;
+  
+    fetch(getLatLonURL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("HTTP error " + response.status);
+          // COMMENT: Append text if there was a failed response.
+          $("#invalidEntry").show();
+          $("#invalidEntry").text(
+            "The response from the weather api failed. Please try again."
+          );
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.length === 0) {
+          // COMMENT: Append text if there is a problem with user input
+          $("#invalidEntry").show();
+          $("#invalidEntry").text(
+            "The weather data for the city you're searching for is not in the database of available cities, the input format was incorrect, is spelled incorrectly, or is not a valid city. Please, try again."
+          );
+        } else {
+          let cityData = data[0];
+          let coordinates = cityData.lat + ',' + cityData.lon;
+          localStorage.setItem(coordinates, JSON.stringify(cityData));
+        }
+      })
+      .catch((error) => {
+        console.error("", error);
+        if (error.message === "HTTP error 400") {
+          $("#invalidEntry").show();
+          $("#invalidEntry").text(
+            "No text was entered or the fetching of the data failed. Please try again"
+          );
+        }
+      });
+  });
+  
 
-let apiKey = "ae8a9c52dbc61de25a46b1c84484bee2";
 
-let lat = "";
-let lon = "";
-
-$("form").on("submit", function (event) {
-  event.preventDefault();
-  $("#invalidEntry").text("");
-  let cityName = $("#cityName").val();
-  let getLatLonURL =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    cityName +
-    "&appid=" +
-    apiKey;
-
-  fetch(getLatLonURL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("HTTP error " + response.status);
-        // COMMENT: Append text if there was a failed response.
-        $("#invalidEntry").show();
-        $("#invalidEntry").text(
-          "The response from the weather api failed. Please try again."
-        );
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data.length === 0) {
-        // COMMENT: Append text if there is a problem with user input
-        $("#invalidEntry").show();
-        $("#invalidEntry").text(
-          "The weather data for the city you're searching for is not in the database of available cities, the input format was incorrect, is spelled incorrectly, or is not a valid city. Please, try again."
-        );
-      } else {
-        console.log(data);
-      }
-    })
-    .catch((error) => {
-      console.error("", error);
-      if (error.message === "HTTP error 400") {
-        $("#invalidEntry").show();
-        $("#invalidEntry").text(
-          "No text was entered or the fetching of the data failed. Please try again"
-        );
-      }
-    });
-});
+// $(document).ready(function () {
+//     $("#invalidEntry").hide();
+//   });
+  
+//   let apiKey = "ae8a9c52dbc61de25a46b1c84484bee2";
+  
+//   let lat = "";
+//   let lon = "";
+//   let coordinates = lat + lon;
+  
+//   $("form").on("submit", function (event) {
+//     event.preventDefault();
+//     $("#invalidEntry").text("");
+//     let cityName = $("#cityName").val();
+//     let getLatLonURL =
+//       "http://api.openweathermap.org/geo/1.0/direct?q=" +
+//       cityName +
+//       "&appid=" +
+//       apiKey;
+  
+//     fetch(getLatLonURL)
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error("HTTP error " + response.status);
+//           // COMMENT: Append text if there was a failed response.
+//           $("#invalidEntry").show();
+//           $("#invalidEntry").text(
+//             "The response from the weather api failed. Please try again."
+//           );
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         if (data.length === 0) {
+//           // COMMENT: Append text if there is a problem with user input
+//           $("#invalidEntry").show();
+//           $("#invalidEntry").text(
+//             "The weather data for the city you're searching for is not in the database of available cities, the input format was incorrect, is spelled incorrectly, or is not a valid city. Please, try again."
+//           );
+//         } else {
+//           console.log(data);
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("", error);
+//         if (error.message === "HTTP error 400") {
+//           $("#invalidEntry").show();
+//           $("#invalidEntry").text(
+//             "No text was entered or the fetching of the data failed. Please try again"
+//           );
+//         }
+//       });
+//   });
